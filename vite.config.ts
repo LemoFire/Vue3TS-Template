44ca12ab1361ce1d0975842fee420455c9204d41
LemoFire <lemofire@gmail.com>
2022-02-09 17:02:32 +0800

    feat:添加vite代理设置

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,13 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/path": {
+        target: "https://domain",
+        changeOrigin: true,
+      },
+    },
+  },
 });
